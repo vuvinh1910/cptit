de1479c23768aee16b06221b48852992fcaa926e
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-07 01:51:23 +0700

    Add files via upload

---
+++ bt5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+main(){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if (b==0){
+		printf("%d",b);
+	} else {
+		printf("%d ",a+b);
+		printf("%d ",a-b);
+		printf("%.2f ",float(a)/float(b));
+		printf("%d ", a % b);
+	}
+	return 0;
+}
