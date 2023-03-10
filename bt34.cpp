f3ef03de41207f020360471dc23b5ed9b7755d19
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-10 21:47:22 +0700

    Add files via upload

---
+++ bt34.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+main(){
+    long int a,b,i,c,d,e,f;
+    scanf("%d",&a);
+    b=a;c=a;
+    while (b--){
+    for (i=1;i<=a;i++){
+        printf("*");
+    } printf("\n");
+    }
+       return 0;
+}
