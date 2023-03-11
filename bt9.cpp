df1d79dd0804fd683260d775962ddf8e751373fe
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-11 15:44:11 +0700

    Update bt9.cpp

--- bt9.cpp
+++ bt9.cpp
@@ -1,17 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 main(){
-	int a,x,b;
-	scanf("%d%d",&a,&b);
-	x=(-b)/a;
-	if (a==0){
-		printf("Vo nghiem");
-	} else {
-		if(a==0 && b==0){
-			printf("Vo so nghiem");
-		} else {
-			printf("%.2f",(float) -b/ (float) a);
-		}
+	int a,b,c,d;
+	scanf("%d",&a);
+	b=a/365;c=(a-365*b)/7;d=a-365*b-7*c;
+	if (a<=1000){
+	printf("%d ",b);
+	printf("%d ",c);
+	printf("%d ",d);
 	}
 	return 0;
 }
