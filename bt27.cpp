921de2ff6ca83af344e72869e4974196ed5eb66e
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-11 16:00:51 +0700

    Update bt27.cpp

--- bt27.cpp
+++ bt27.cpp
@@ -2,13 +2,10 @@
 #include<math.h>
 main(){
     int a,b,i,c,d,e;
-    int chan=0;
-    int le=0;
     scanf("%d",&a);
-    while (a>0){
-        b=a%10;
+    b= a%10;
+    while(a>=10){
         a=a/10;
-            if(b%2==0){chan=chan+1;} else {le=le+1;}
-    }printf("%d %d",le,chan);
+    } printf("%d %d",a,b);
     return 0;
 }
