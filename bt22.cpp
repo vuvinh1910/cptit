463aaee6f23e7e9981ee5bab578dc5ea956cb6a7
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-11 15:55:40 +0700

    Update bt22.cpp

--- bt22.cpp
+++ bt22.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 main(){
-    int a,b,i,c,d,e;
-    scanf("%d",&a);
-    e=a;
-    c=0;
-    i= a % 10;
-    while(e>10){
-        c++;
-        e=e/10;
-    } d= a+(i-e)*pow(10,c)-i+e;
-    printf("%d",d);
+    int a,b,i,c,d;
+    scanf("%d%d",&a,&b);
+    d=0;
+    if (a<=b){
+        for (i=a;i<=b;i++){
+        c=i;d+=c;} printf("%d",d);
+    } else {
+        for (i=b;i<=a;i++){
+        c=i;d+=c;} printf("%d",d);
+    }
     return 0;
 }
