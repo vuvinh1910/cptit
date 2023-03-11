57b6d8fc1dd0e7b37c470336dfbccba5a46a6cad
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-11 15:57:55 +0700

    Update bt24.cpp

--- bt24.cpp
+++ bt24.cpp
@@ -3,14 +3,13 @@
 main(){
     int a,b,i,c,d,e;
     scanf("%d",&a);
-    i=2;c=1;d=a;
-    while (a!=1){
-        if (a%i==0){
-            c=c+i;
-            a=a/i;
-        } else {
-            i=i+1;
-        }
-    } if (d==c){printf("1");} else{printf("0");}
+    e=a;
+    c=0;
+    i= a % 10;
+    while(e>0){
+        c++;
+        e=e/10;
+    } d= a+(i-e)*pow(10,c)-i+e;
+    printf("%d",c);
     return 0;
 }
