4aafcafbfed45b345ede205dbc3e59b67d41a4f5
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-11 15:41:12 +0700

    Update bt5.cpp

--- bt5.cpp
+++ bt5.cpp
@@ -3,13 +3,6 @@
 main(){
 	int a,b;
 	scanf("%d %d",&a,&b);
-	if (b==0){
-		printf("%d",b);
-	} else {
-		printf("%d ",a+b);
-		printf("%d ",a-b);
-		printf("%.2f ",float(a)/float(b));
-		printf("%d ", a % b);
-	}
+	printf("%d",a+b);
 	return 0;
 }
