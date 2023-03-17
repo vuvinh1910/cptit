fce1de0a0634adc82029b0fe0b0e1b105cfbc2a8
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-18 02:21:10 +0700

    Add files via upload

---
+++ bt60.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a,b,c,d,e,f,g,h,i,k,l,m,n;
+    scanf("%d",&a);
+    for (b=5;b<=a;b++){
+        if (b==6||b==28||b==496||b==8128||b==33550336||b==8589869056)
+        printf("%d ",b);
+    }
+}
