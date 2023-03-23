73c2f44037252c4c3730e61a1f959d668baf41b5
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-23 19:35:22 +0700

    Delete bt17test.cpp

--- bt17test.cpp
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
- #include <math.h>
-  int kiemtraSNT(int x){ if(x<2) return 0; for(int i=2;i<=sqrt(x);i++){ if(x%i==0) return 0; } return 1; } int main(){ int n,t; scanf("%d",&t); while(t--){ scanf("%d",&n); int kt= kiemtraSNT(n); if(kt==0) printf("NO\n"); else printf("YES\n"); } }
