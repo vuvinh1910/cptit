de0772040fe19ce712d1c3f1efa90ad3b9fbaf07
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2023-03-26 02:09:13 +0700

    Update bt113.cpp

--- bt113.cpp
+++ bt113.cpp
@@ -3,29 +3,21 @@
 int main(){
 	int b,c[100],d,e,f,g[100],h,i,k[100],l,m,n,max,min;
     int t,a;
-        scanf("%d",&t);
-        while(t--){
-            scanf("%d",&a);
-            for(b=0;b<a;b++){
-                scanf("%d",&c[b]);
-            }
-            for(b=0;b<a;b++){
-                g[b]=1;
-                for(d=b+1;d<a;d++){
-                    if(b==a-1){
-                        g[b]=1;break;
-                    } else {
-                    if(c[b]<=c[d]){
-                        g[b]=0;break;
-                    }
-                    }
+        scanf("%d",&a);
+        for(b=0;b<a;b++){
+            scanf("%d",&c[b]);
+        }
+        for(b=0;b<a;b++){
+            for(d=b+1;d<a;d++){
+                if(c[b]==c[d]){
+                    g[d]=-1;
                 }
             }
-            for(b=0;b<a;b++){
-                if(g[b]==1){
-                    printf("%d ",c[b]);
-                }
-            } printf("\n");
+        }
+        for(b=0;b<a;b++){
+            if(g[b]!=-1){
+            printf("%d ",c[b]);
+            }
         }
     return 0;
 }
